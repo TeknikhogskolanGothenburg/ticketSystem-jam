da98e200307e14cef60557a874aab2ac34a0b0ec
Jakob <32323708+jakobpucher@users.noreply.github.com>
2018-02-06 13:59:58 +0100

    Lagt till några API-Controllers

---
+++ src/TicketAPI/Controllers/TicketEventsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/TicketEvents")]
+    public class TicketEventsController : Controller
+    {
+        // GET: api/TicketEvents
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/TicketEvents/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/TicketEvents
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/TicketEvents/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
