9a8ae52136cd23d44ef4f02823df821ed95854af
Jakob <32323708+jakobpucher@users.noreply.github.com>
2018-02-07 13:38:48 +0100

    div

--- src/TicketAPI/Controllers/VenuesController.cs
+++ src/TicketAPI/Controllers/VenuesController.cs
@@ -9,15 +9,13 @@ namespace TicketAPI.Controllers
     [Route("api/Venues")]
     public class VenuesController : Controller
     {
-        
-        private TicketDatabase tdb;
-
+        TicketDatabase tdb = new TicketDatabase();
+ 
         // GET: api/Venues
         [HttpGet]
         public List<Venue> Get()
         {
-            TicketDatabase v = new TicketDatabase();
-            List<Venue> values = v.VenuesFindAll();
+            List<Venue> values = tdb.VenuesFindAll();
             return values;
         }
 
@@ -25,10 +23,8 @@ namespace TicketAPI.Controllers
         [HttpGet("{id}")]
         public Venue Get(string id)
         {
-            TicketDatabase v = new TicketDatabase();
-            Venue venue = v.VenuesFind(id);
+            Venue venue = tdb.VenuesFind(id);
             return venue;
-
         }
          
 
@@ -39,8 +35,7 @@ namespace TicketAPI.Controllers
         /// <param name="values"></param>
         [HttpPost]
         public void Post([FromBody]Venue values)
-        {
-            tdb = new TicketDatabase();          
+        {         
             tdb.VenueAdd(values.VenueName, values.Address, values.City, values.Country);
 
             // Förslag på inputsträngar. OBS! ANVÄND INTE Å Ä Ö, med nedan kod.
