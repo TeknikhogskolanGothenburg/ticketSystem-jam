f1438313c38be09d13cd2228d3b52be609cd17b9
Jakob <32323708+jakobpucher@users.noreply.github.com>
2018-02-11 10:35:51 +0100

    ok

--- src/ClassLibrary/Venue.cs
+++ src/ClassLibrary/Venue.cs
@@ -15,6 +15,7 @@ namespace ClassLibrary
         public string Address { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public int Seats { get; set; }
     }
 }
