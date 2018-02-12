31a187122d89d8b031e7948c7bf413e7de4e0294
Agneta Gustafsson <agneta.kode@outlook.com>
2018-02-12 13:53:50 +0100

    lite language

--- src/TicketAPI/Startup.cs
+++ src/TicketAPI/Startup.cs
@@ -23,6 +23,7 @@ namespace TicketAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           // services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.AddMvc();
         }
 
