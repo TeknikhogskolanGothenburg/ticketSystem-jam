0be119c4964051d376515c8c35008a281565ca7d
mmmolin78 <mmmolin78@gmail.com>
2018-02-12 09:09:54 +0100

    Added TicketTransactions class in ClassLibrary

---
+++ src/ClassLibrary/TicketTransactions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class TicketTransactions
+    {
+        int TransactionID { get; set; }
+        string BuyerLastName { get; set; }
+        string BuyerFirstName { get; set; }
+        string BuyerAddress { get; set; }
+        string BuyerCity { get; set; }
+        string PaymentStatus { get; set; }
+        string PaymentReferenceId { get; set; }
+    }
+}
