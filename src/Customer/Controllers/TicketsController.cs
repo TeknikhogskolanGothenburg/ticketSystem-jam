67f361adeac5675df8405fcc9d943e562bb8eb76
Jakob <32323708+jakobpucher@users.noreply.github.com>
2018-02-13 14:20:53 +0100

    FrontEnd

---
+++ src/Customer/Controllers/TicketsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Customer.Models;
+using ClassLibrary;
+using TicketSystem.RestApiClient;
+using System.Resources;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Localization;
+
+namespace Customer.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private static Value value;
+        private static TicketApi ticketApi;
+        public SeatsAtEventDate SeatsAtEventDate;
+        public TicketEvent TicketEvent;
+   
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Tickets()
+        {
+            if (value == null)
+            {
+                value = new Value();
+            }
+            if (ticketApi == null)
+            {
+                ticketApi = new TicketApi();
+            }
+
+            value.Events = ticketApi.GetAllEvents();
+            return View(value);
+        }
+
+        public IActionResult GetAllEventDates()
+        {
+            ticketApi.GetAllEventDates();
+            return View(value);
+        }
+
+
+    }
+}
