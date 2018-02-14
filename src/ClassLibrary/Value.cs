19f276ff5afb799287aa4d0b98d18c9394cec91d
mmmolin78 <mmmolin78@gmail.com>
2018-02-14 20:18:21 +0100

    html stuff

--- src/ClassLibrary/Value.cs
+++ src/ClassLibrary/Value.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ClassLibrary
 {
-    public class Value
+    public class Value // HERREJÄVLAR vad mycket skit det ligger här nu, vi får rensa.
     {
         public List<Venue> Venues { get; set; } = new List<Venue>();
 
@@ -17,12 +17,11 @@ namespace ClassLibrary
 
         public List<Tickets> BookTicket = new List<Tickets>();
 
-        public List<TicketEventDate> TicketEventDate { get; set; } = new List<TicketEventDate>(); //behöver lite att testa med..
+        public List<TicketEventDate> TicketEventDate { get; set; } = new List<TicketEventDate>();
 
-        public List<TicketTransaction> TicketTransaction { get; set; } = new List<TicketTransaction>(); //..och lite mer.
+        public List<TicketTransaction> TicketTransaction { get; set; } = new List<TicketTransaction>();
 
-<<<<<<< HEAD
-        public List<EventSummary> EventSummaries { get; set; } = new List<EventSummary>(); // ...nu nu .. nu är det den sista.
+        public List<EventSummary> EventSummaries { get; set; } = new List<EventSummary>();
 
         public TicketEventDate TicketEventDates { get; set; }
 
@@ -31,18 +30,13 @@ namespace ClassLibrary
         public List<EventSummary> CartSummary { get; set; } = new List<EventSummary>();
 
         public int Id { get; set; }
-=======
-        public List<EventSummary> EventSummarylist { get; set; } = new List<EventSummary>(); // ...nu nu .. nu är det den sista.
 
-        public TicketEventDate TicketEventDates { get; set; }
+        public List<EventSummary> EventSummarylist { get; set; } = new List<EventSummary>();
 
-       public EventSummary EventSummaryprop { get; set; }
+        public EventSummary EventSummaryprop { get; set; }
 
-     public    List<EventSummary> sum { get; set; }
+        public List<EventSummary> sum { get; set; }
+
         public List<EventSummary> shoppingCart { get; set; }
-
-
-
->>>>>>> upstream/master
     }
 }
