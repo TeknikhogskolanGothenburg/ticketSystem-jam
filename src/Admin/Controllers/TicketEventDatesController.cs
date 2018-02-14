47b91ddbbf34bf2fb11f69de139cae86c69002a0
mmmolin78 <mmmolin78@gmail.com>
2018-02-14 12:41:44 +0100

    Added TicketEventDates Controller

---
+++ src/Admin/Controllers/TicketEventDatesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ClassLibrary;
+using TicketSystem.RestApiClient;
+
+namespace Admin.Controllers
+{
+    public class TicketEventDatesController : Controller
+    {
+        TicketApi ticketApi = new TicketApi();
+        Value value = new Value();
+
+        // GET: TicketEventDates
+        public ActionResult TicketEventDates()
+        {
+            if (value == null)
+            {
+                value = new Value();
+            }
+            if (ticketApi == null)
+            {
+                ticketApi = new TicketApi();
+            }
+            value.Venues = ticketApi.GetAllVenues();
+            return View(value);
+        }
+
+        // GET: TicketEventDates/Details/5
+        public ActionResult AddTicketEventDate(TicketEventDate ticketEventDate)
+        {
+            return View();
+        }
+
+    }
+}
